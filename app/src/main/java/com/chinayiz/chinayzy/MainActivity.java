dec40ba754786012c5e84f2fbd0cb23c3483a172
canrom7 <canrom7@outlook.com>
2016-12-26 17:26:34 +0800

    test push

--- app/src/main/java/com/chinayiz/chinayzy/MainActivity.java
+++ app/src/main/java/com/chinayiz/chinayzy/MainActivity.java
@@ -3,9 +3,11 @@ package com.chinayiz.chinayzy;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Context mContext;
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
