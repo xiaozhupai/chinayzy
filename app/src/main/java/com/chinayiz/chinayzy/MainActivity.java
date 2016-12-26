5bd63c1a74f7dd9d117bb6f52b38f2b5b0da519b
canrom7 <canrom7@outlook.com>
2016-12-26 17:22:03 +0800

    test

--- app/src/main/java/com/chinayiz/chinayzy/MainActivity.java
+++ app/src/main/java/com/chinayiz/chinayzy/MainActivity.java
@@ -1,13 +1,15 @@
 package com.chinayiz.chinayzy;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
