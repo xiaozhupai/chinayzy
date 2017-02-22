9ccbaa7c8ffbca30a04e5bdc150f9f200c62fd66
canrom7 <canrom7@outlook.com>
2017-02-22 14:23:09 +0800

    Delete All

--- app/src/main/java/com/chinayiz/chinayzy/adapter/GoodStandardAdaphter.java
+++
@@ -1,8 +0,0 @@
-package com.chinayiz.chinayzy.adapter;
-
-/**
- * Created by Administrator on 2017/2/16.
- */
-
-public class GoodStandardAdaphter {
-}
--- app/src/main/java/com/chinayiz/chinayzy/entity/response/UserModel.java
+++
@@ -1,9 +0,0 @@
-package com.chinayiz.chinayzy.entity.response;
-
-/**
- * Created by Administrator on 2017/1/17.
- */
-
-public class UserModel {
-
-}
